2177ea2127fd0b63a9dd7934a9a0654faa9f11aa
TheBitson124 <161846390+TheBitson124@users.noreply.github.com>
2025-10-17 19:00:12 +0200

    Finally Enemy Detection

--- Assets/Scripts/MeleeCollider.cs
+++ Assets/Scripts/MeleeCollider.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MeleeCollider : MonoBehaviour
 {
     List<BasicEnemy> enemies;
+    public Transform PlayerOrientation;
     public List<BasicEnemy> GetEnemies() { return enemies; }
     void Start()
     {
         enemies = new List<BasicEnemy>();
     }
+    private void Update()
+    {
+        transform.position = PlayerOrientation.position + PlayerOrientation.forward;
+        Collider[] hitColliders = Physics.OverlapBox(transform.position, Vector3.Scale(GetComponent<BoxCollider>().size * 0.5f, transform.lossyScale), PlayerOrientation.rotation);
+        Debug.Log(hitColliders);
+
+    }
     private void OnTriggerEnter(Collider other)
     {
-        print(other.name);
+        Debug.Log($"Trigger entered with: {other.name} on layer {other.gameObject.layer}");
         if (other.TryGetComponent<BasicEnemy>(out BasicEnemy enemy))
         {
             if (!enemies.Contains(enemy))
@@ -32,5 +41,20 @@ public class MeleeCollider : MonoBehaviour
         }
        
     }
-
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (GetComponent<BoxCollider>())
+        {
+            BoxCollider box = GetComponent<BoxCollider>();
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawWireCube(box.center, box.size);
+        }
+        else if (GetComponent<SphereCollider>())
+        {
+            SphereCollider sphere = GetComponent<SphereCollider>();
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawWireSphere(sphere.center, sphere.radius);
+        }
+    }
 }
--- Assets/Scripts/PlayerCombatController.cs
+++ Assets/Scripts/PlayerCombatController.cs
@@ -5,13 +5,55 @@ using UnityEngine;
 
 public class PlayerCombatController : MonoBehaviour
 {
-    MeleeCollider _meleeCollider;
-    void Start()
-    {
-        _meleeCollider = GetComponentInChildren<MeleeCollider>();
-    }
+    public float detectionRange = 3f;
+    public float detectionWidth = 2f;
+    public float detectionHeight = 2f;
+    public bool showDebugGizmos = true;
+    private List<BasicEnemy> detectedEnemies = new List<BasicEnemy>();
+    private Collider[] detectionBuffer = new Collider[20];
+
     void Update()
     {
-        //print(_meleeCollider.GetEnemies().Count);
+        detectedEnemies.Clear();
+
+        Vector3 detectionCenter = transform.position + transform.forward * (detectionRange * 0.5f);
+        Vector3 halfExtents = new Vector3(detectionWidth * 0.5f, detectionHeight * 0.5f, detectionRange * 0.5f);
+
+        int numColliders = Physics.OverlapBoxNonAlloc(
+            detectionCenter,
+            halfExtents,
+            detectionBuffer,
+            transform.rotation
+        );
+        for (int i = 0; i < numColliders; i++)
+        {
+            if (detectionBuffer[i].TryGetComponent<BasicEnemy>(out BasicEnemy enemy))
+            {
+                if (!detectedEnemies.Contains(enemy))
+                {
+                    detectedEnemies.Add(enemy);
+                }
+            }
+        }
+        print(detectedEnemies.Count);
+    }
+    private void OnDrawGizmos()
+    {
+        if (!showDebugGizmos) return;
+
+        Gizmos.color = detectedEnemies.Count > 0 ? Color.red : Color.green;
+        Gizmos.matrix = Matrix4x4.TRS(
+            transform.position + transform.forward * (detectionRange * 0.5f),
+            transform.rotation,
+            Vector3.one
+        );
+        Gizmos.DrawWireCube(Vector3.zero, new Vector3(detectionWidth, detectionHeight, detectionRange));
+
+        Gizmos.matrix = Matrix4x4.identity;
+        foreach (BasicEnemy enemy in detectedEnemies)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, enemy.transform.position);
+        }
     }
 }
